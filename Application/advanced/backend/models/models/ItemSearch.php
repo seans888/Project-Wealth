4173617582f5f4e65595d1876d977917073a1ebf
Felix Gregorio III <peliksgreg@gmail.com>
2016-08-18 22:44:05 +0800

    Add files via upload

---
+++ Application/advanced/backend/models/models/ItemSearch.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use backend\models\Item;
+
+/**
+ * ItemSearch represents the model behind the search form about `backend\models\Item`.
+ */
+class ItemSearch extends Item
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['item_id', 'item_name', 'selling_price', 'unit_cost'], 'integer'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Item::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'item_id' => $this->item_id,
+            'item_name' => $this->item_name,
+            'selling_price' => $this->selling_price,
+            'unit_cost' => $this->unit_cost,
+        ]);
+
+        return $dataProvider;
+    }
+}
