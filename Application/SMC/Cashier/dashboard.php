2ec4f91eaf16215bd2176389103fde8338c71175
armersson <armersson@gmail.com>
2016-12-08 09:15:46 +0800

    dashboard.php

---
+++ Application/SMC/Cashier/dashboard.php
@@ -0,0 +1,159 @@
+<?php require_once 'includes/header2.php'; ?>
+
+<?php 
+
+$sql = "SELECT * FROM product WHERE status = 1";
+$query = $connect->query($sql);
+$countProduct = $query->num_rows;
+
+$orderSql = "SELECT * FROM orders WHERE order_status = 1";
+$orderQuery = $connect->query($orderSql);
+$countOrder = $orderQuery->num_rows;
+
+$totalRevenue = "";
+while ($orderResult = $orderQuery->fetch_assoc()) {
+	$totalRevenue += $orderResult['paid'];
+}
+
+$lowStockSql = "SELECT * FROM product WHERE quantity <= 3 AND status = 1";
+$lowStockQuery = $connect->query($lowStockSql);
+$countLowStock = $lowStockQuery->num_rows;
+
+$connect->close();
+
+?>
+
+	<div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Dashboard <small>Cashier Overview</small>
+                        </h1>
+                        <ol class="breadcrumb">
+                            <li class="active">
+                                <i class="fa fa-dashboard"></i> Dashboard
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+                <!-- /.row -->
+
+           
+                <div class="row">
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-ruble fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <div class="huge"><?php if($totalRevenue) {
+															echo $totalRevenue;
+															} else {
+																echo '0';
+																} ?></div>
+                                        <div>Total Revenue</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="orders.php?o=manord">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-green">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-gift fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <div class="huge"><?php echo $countProduct; ?></div>
+                                        <div>Total Products</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="product.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Products</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-yellow">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-shopping-cart fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <div class="huge"><?php echo $countOrder; ?></div>
+                                        <div>Total Orders</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="orders.php?o=manord">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Orders</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-red">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-plus fa-5x"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="orders.php?o=add">
+                                <div class="panel-footer">
+                                    <span class="pull-left">Add Orders</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.row -->
+                 
+                   
+            
+
+               
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+
+<!-- fullCalendar 2.2.5 -->
+<script src="assests/plugins/moment/moment.min.js"></script>
+<script src="assests/plugins/fullcalendar/fullcalendar.min.js"></script>
+
+  <!-- custom css -->
+  <link rel="stylesheet" href="custom/css/custom.css">	
+
+
+
+
+<?php require_once 'includes/footer.php'; ?>
